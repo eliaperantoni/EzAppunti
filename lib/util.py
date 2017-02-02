01ef114246e30800aca8342beb088550c619f8ae
hellix08 <hellix08.tubo@gmail.com>
2017-02-02 22:46:42 +0100

    Previous commit ignored lib folder, fixed .gitignore

---
+++ lib/util.py
@@ -0,0 +1,30 @@
+import sys
+from ftplib import FTP
+def db_edit(file,line,index,new):
+    with open(file,"r") as f:
+        lines = f.readlines()
+    items = lines[line].split(";")
+    out=""
+    x=-1
+    for i in items:
+        x+=1
+        if(x==index):
+            out+=new
+        else:
+            out+=items[x]
+        if not(items[x]==items[-1]):
+            out+=";"
+    lines[line]=out
+    with open(file, "w") as f:
+        f.writelines(lines)
+
+def exitProgram():
+    input("Press enter to exit ")
+    sys.exit(0)
+host = "f14-preview.royalwebhosting.net"
+pasw = "tooezforrtz"
+user = "2289107"
+def connectToServer():
+    ftp = FTP(host)
+    ftp.login(user,pasw)
+    return ftp
